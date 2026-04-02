ee6edabb4884d47ee88f32556389b7f9c87fceaa
faenuccio <filippo.nuccio@univ-st-etienne.fr>
2026-04-02 22:56:32 +0200

    finished Sets&Filters

--- GradCourse26/5_Sets&Limits.lean
+++ GradCourse26/5_Sets&Limits.lean
@@ -1,7 +1,7 @@
 import Mathlib.Tactic
 
 open Set Classical
-
+open Filter Topology
 
 -- # §1: Sets
 
@@ -11,87 +11,59 @@ section Definitions
 
 -- **An error**
 example (S : Set) := sorry
-example {α : Type} (S : Set α) : S = S := by sorry
+example {α : Type} (S : Set α) : S = S := sorry
 
 -- `⌘`
 
 -- **A tautology**
-
-example (α : Type) (x : α) (S : Set α) : x ∈ S ↔ S x := by
-  sorry
+example (α : Type) (x : α) (S : Set α) : x ∈ S ↔ S x := sorry
 
 
 -- **The positive integers**
-
-def PositiveIntegers : Set ℤ := by
-  sorry
+def PositiveIntegers : Set ℤ := sorry
 
 
-lemma one_posint : 1 ∈ PositiveIntegers := by
-  sorry
-
-def PositiveNaturals : Set ℕ := by
-  sorry
-
-
-example : 1 ∈ PositiveNaturals := by
-  sorry
-
-
--- Why does this *fail*? How to fix it?
-example : (-1) ∉ PositiveNaturals := sorry
-
+lemma one_posint : 1 ∈ PositiveIntegers := sorry
 
 -- **The even naturals**
+def EvenNaturals : Set ℕ := sorry
 
-def EvenNaturals : Set ℕ := by
-  sorry
-
-example (n : ℕ) : n ∈ EvenNaturals → (n + 2) ∈ EvenNaturals := by
-  sorry
+example (n : ℕ) : n ∈ EvenNaturals → (n + 2) ∈ EvenNaturals := sorry
 
 
 -- **An abstract set**
-
 def AbstractSet {α : Type} (P : α → Prop) : Set α := P
 def AbstractSet' {α : Type} (P : α → Prop) : Set α := setOf P
 
 /- The same, but it is a general principle that the second version is better because it
 avoids abusing `defeq`. -/
-example {α : Type} (P : α → Prop) : AbstractSet P = AbstractSet' P := by
-  sorry
+example {α : Type} (P : α → Prop) : AbstractSet P = AbstractSet' P := sorry
 
 
 -- `⌘`
 
 
 -- **Subsets as implication**
-example {α : Type} (S T : Set α) (s : α) (hST : S ⊆ T) (hs : s ∈ S) : s ∈ T := by
-  sorry
+example {α : Type} (S T : Set α) (s : α) (hST : S ⊆ T) (hs : s ∈ S) : s ∈ T := sorry
 
 
 -- **A double inclusion**
+example (α : Type) (S T W : Set α) (hST : S ⊆ T) (hTW : T ⊆ W) : S ⊆ W := sorry
 
-example (α : Type) (S T W : Set α) (hST : S ⊆ T) (hTW : T ⊆ W) : S ⊆ W := by
-  sorry
-
-
--- **Another take on subsets and sets as types**
-
-def subsub {α : Type} {S : Set α} (P : S → Prop) : Set (S : Type) := P
-
-def subsub' {α : Type} {S : Set α} (P : α → Prop) : Set (S : Type) := by
-  sorry
-
--- Are they *equal*? This is an exercise below.
 
 -- Why does this *fail*? How to fix it?
-example (α : Type) (S : Set α) (P : S → Prop) (x : ↑S) (hx : x ∈ subsub P) : x ∈ S := sorry
+-- example : ∀ n : PositiveIntegers, 0 ≤ n := sorry
+/- *Sol.:*  This fails because `0` is a term of `ℕ`, whereas `n` is a term of `PositiveIntegers`.
+They cannot be compared directly, because `n` is actually a *pair* of a natural number and a proof
+of its positivity. It can be made to work as follows-/
+example : ∀ n : PositiveIntegers, 0 < n.1 := sorry
 
--- Why does this *fail*? How to fix it?
-example : ∀ n : PositiveIntegers, 0 ≤ n := sorry
+--Sets can be interpreted as types on their own
+def rest (α β : Type*) (f : α → β) (S : Set β) (h : ∀ a : α, f a ∈ S) : α → S := sorry
 
-
+-- This is enforced through the
+variable (α : Type*) in
+#synth CoeSort (Set α) (Type _)
 
 -- `⌘`
 
@@ -102,192 +74,220 @@ section Operations
 -- ## Operations on sets
 
 -- **Self-intersection is the identity, proven with extensionality**
-
-example (α : Type) (S : Set α) : S ∩ S = S := by
-  sorry
+example (α : Type) (S : Set α) : S ∩ S = S := sorry
 
 
 -- **The union**
-
-example (α : Type) (S T : Set α) (H : S ⊆ T) : S ∪ T = T := by
-  sorry
+example (α : Type) (S T : Set α) (H : S ⊆ T) : S ∪ T = T := sorry
 
 
 -- **An _unfixable_ problem**
-
 example (α β : Type) (S : Set α) (T : Set β) : S ⊆ S ∪ T := sorry
-
 
 
 -- `⌘`
 
 
 -- **Empty set**
+example : (setOf (0 < ·) : Set ℤ) ∩ setOf (· < 0) = ∅ := sorry
 
-example : (setOf (0 < ·) : Set ℤ) ∩ setOf (· < 0) = ∅ := by
-  sorry
 
 -- `⌘`
 
 -- **§ Indexed unions**
-
-example {α I : Type} (A : I → Set α) (x : α) : x ∈ ⋃ i, A i ↔ ∃ i, x ∈ A i := by
-  sorry
-
+example {α I : Type} (A : I → Set α) (x : α) : x ∈ ⋃ i, A i ↔ ∃ i, x ∈ A i := sorry
 
 -- `⌘`
 
 end Operations
 
-section Functions
+
+section Filter
 
 
--- # §2: Functions
+variable {α β γ : Type*}
+
+lemma mono (f : α → β) {F G : Filter α} (h : F ≤ G) : F.map f ≤ G.map f := sorry
 
 
--- Functions do not natively act on elements of sets: how can we fix this code?
-example (α β : Type) (S : Set α) (T : Set β) (f g : S → β) :
-  f = g ↔ ∀ a : α, a ∈ S → f a  = g a := by sorry
+/-- The function `f` tends to `G` along `F`. -/
+def Tendsto_ENS (f : α → β) (F : Filter α) (G : Filter β) := F.map f ≤ G
+#print Tendsto
+
+lemma Tendsto_comp {s : α → β} {t : β → γ} {F : Filter α} {G : Filter β} {H : Filter γ}
+    (hs : Tendsto s F G) (ht : Tendsto t G H) : Tendsto (t ∘ s) F H := sorry
+
+
+example (f g : ℝ → ℝ) (hf : Tendsto f (𝓝 0) (𝓝 Real.pi))
+    (hg : Tendsto g (𝓝 Real.pi) atTop) : Tendsto (g ∘ f) (𝓝 0) atTop := sorry
+
+
+example (a : ℕ → ℝ) (φ : ℝ → ℂ) (ha : Tendsto a atTop (𝓝 (-1)))
+    (hφ : Tendsto φ (𝓝 (-1)) (𝓝 (Complex.I))) : Tendsto (φ ∘ a) atTop (𝓝 (Complex.I)) := sorry
+
+/-
+`filter_upwards [h₁, ⋯, hₙ]` replaces a goal of the form `s ∈ f` and terms
+`h₁ : t₁ ∈ f, ⋯, hₙ : tₙ ∈ f` with `∀ x, x ∈ t₁ → ⋯ → x ∈ tₙ → x ∈ s`.
+The list is an optional parameter, `[]` being its default value.
+
+`filter_upwards [h₁, ⋯, hₙ] with a₁ a₂ ⋯ aₖ` is a short form for
+`{ filter_upwards [h₁, ⋯, hₙ], intro a₁ a₂ ⋯ aₖ }`.
+
+`filter_upwards [h₁, ⋯, hₙ] using e` is a short form for
+`{ filter_upwards [h1, ⋯, hn], exact e }`.
+
+Combining both shortcuts is done by writing `filter_upwards [h₁, ⋯, hₙ] with a₁ a₂ ⋯ aₖ using e`.
+Note that in this case, the `aᵢ` terms can be used in `e`.
+-/
+
+example [TopologicalSpace α] {x : α} {p p' q q' : α → Prop}
+    (hT : {x | p x} ∈ 𝓝 x)
+    (hT' : {x | p' x} ∈ 𝓝 x)
+    (hS : {x | q x} ∈ 𝓝 x)
+    (hS' : {x | q' x} ∈ 𝓝 x) :
+    {x | p x ∧ q x ∨ p' x ∧ q' x} ∈ 𝓝 x := sorry
 
 -- `⌘`
 
-variable (α β : Type)
 
--- The **image**
+section EventuallyFrequently
 
-example : 1 ∈ Nat.succ '' univ := by
-  sorry
+lemma EventuallyLTOne : ∀ᶠ x in 𝓝 (0 : ℝ), |x| < 1 := sorry
 
 
--- We can upgrade a function `f` to a function between sets, using its image:
-example (f : α → β) : Set α → Set β := by
-  sorry
+example : ∃ᶠ (n : ℕ) in atTop, Even n := sorry
 
-
-example (α β : Type) (f : α → β) (S : Set α) : S ≠ ∅ → f '' S ≠ ∅ := by
-  sorry
-
--- The **preimage**
-
-example : 2 ∈ Nat.succ ⁻¹' {2, 3} ∧ 1 ∉ .succ ⁻¹' {0, 3} := by
-  sorry
-
+end EventuallyFrequently
 
 -- `⌘`
 
--- **Injectivity**
-
-example : InjOn (fun n : ℤ ↦ n ^ 2) PositiveIntegers := by
-  sorry
+section Limit
 
 
-end Functions
+-- Some classical limits
+example : Tendsto (fun n : ℕ ↦ 1 / (n : ℝ)) atTop (𝓝 0) := sorry
+
+
+-- The pleasure of division in `ℕ`:
+example : Tendsto (fun n : ℕ ↦ 1 / n) atTop (𝓝 0) := sorry
+
+
+#check Tendsto.congr'
+
+#check Filter.eventually_ne_atTop
+
+example : Tendsto (fun n : ℕ ↦ (n + 1 : ℝ) / n) atTop (𝓝 1) := sorry
+
+
+theorem lemma1 : Tendsto (fun n : ℕ ↦ n ^ 2) atTop atTop := sorry
+
+theorem lemma2 : Tendsto (fun n : ℕ ↦ n ^ 2 + n) atTop atTop := sorry
+
+-- Squeeze theorem(s)
+#check tendsto_of_tendsto_of_tendsto_of_le_of_le
+#check tendsto_of_tendsto_of_tendsto_of_le_of_le'
+
+example : Tendsto (fun n : ℕ ↦ ((n : ℝ) ^ 2 + 4 * Real.sqrt n) / (n ^ 2)) atTop (𝓝 1) := sorry
+
+example (f : ℝ → ℝ) (g : ℝ → ℝ) (a l : ℝ) (hf : Tendsto f (𝓝 a) (𝓝 l)) (h : f = g) :
+    Tendsto g (𝓝 a) (𝓝 l) := sorry
+
+-- Congruence for limits
+example (f : ℝ → ℝ) (g : ℝ → ℝ) (a l : ℝ) (hf : Tendsto f (𝓝 a) (𝓝 l)) (h : f =ᶠ[𝓝 a] g) :
+    Tendsto g (𝓝 a) (𝓝 l) := sorry
+
+end Limit
+
+
+end Filter
 
 section Exercises
 
--- **Some exercises**
 
-example : 1 ∉ EvenNaturals := by
-  sorry
+example : 1 ∉ EvenNaturals := sorry
 
 
-example : -1 ∉ PositiveIntegers := by
-  sorry
-
+example : -1 ∉ PositiveIntegers := sorry
 
 -- Define the set of even, positive numbers
-def EvenPositiveNaturals : Set PositiveIntegers := by
-  sorry
+def EvenPositiveNaturals : Set PositiveIntegers := sorry
 
 
 -- Why does this *fail*? How to fix it?
 example : 1 ∉ EvenPositiveNaturals := sorry
 
-
 -- Define the set of odd numbers and prove some properties
 def OddNaturals : Set ℕ := sorry
 
 -- Your definition will be right if the following compiles:
-example : 3 ∈ OddNaturals := by
-  rfl
+example : 3 ∈ OddNaturals := sorry
 
 
-example (n : ℕ) : n ∈ OddNaturals ↔ n ∉ EvenNaturals := by
-  sorry
+example (n : ℕ) : n ∈ OddNaturals ↔ n ∉ EvenNaturals := sorry
+
+-- Consider the following two definigions
+def subsub {α : Type} {S : Set α} (P : S → Prop) : Set (S : Type) := P
+def subsub' {α : Type} {S : Set α} (P : α → Prop) : Set (S : Type) := by
+  intro a
+  exact P a
 
 -- Why does this *fail*?
 example (α : Type) (S : Set α) : subsub = subsub' := sorry
 
 
 -- Try to prove the statement proven before, but without using the library
-example (α : Type) (S T : Set α) (H : S ⊆ T) : T = S ∪ T := by
-  sorry
+example (α : Type) (S T : Set α) (H : S ⊆ T) : T = S ∪ T := sorry
+
+example (α : Type) (S T R : Set α) : S ∩ (T ∪ R) = (S ∩ T) ∪ (S ∩ R) := sorry
 
 
-example (α : Type) (S T R : Set α) : S ∩ (T ∪ R) = (S ∩ T) ∪ (S ∩ R) := by
-  sorry
-
-
-example (α : Type) (S : Set α) : Sᶜ ∪ S = univ := by
-  sorry
-
+example (α : Type) (S : Set α) : Sᶜ ∪ S = univ := sorry
 
 -- For this, you can try `simp` at a certain point...`le_antisymm` can also be useful.
-example : (setOf (0 ≤ ·) : Set ℤ) ∩ setOf (· ≤ 0) = {0} := by
-  sorry
+example : (setOf (0 ≤ ·) : Set ℤ) ∩ setOf (· ≤ 0) = {0} := sorry
 
 
 -- Using your definition of `OddNaturals` prove the following:
-example : EvenNaturals ∪ OddNaturals = univ := by
-  sorry
+example : EvenNaturals ∪ OddNaturals = univ := sorry
+
 
 -- **§** A bit of difference, inclusion and intersection
+example (α : Type) (S T : Set α) (h : T ⊆ S) : T \ S = ∅ := sorry
 
-example (α : Type) (S T : Set α) (h : T ⊆ S) : T \ S = ∅ := by
-  sorry
 
-example (α : Type) (S T R : Set α) : S \ (T ∪ R) ⊆ (S \ T) \ R := by
-  sorry
+example (α : Type) (S T R : Set α) : S \ (T ∪ R) ⊆ (S \ T) \ R := sorry
+
 
 -- Indexed intersections work as indexed unions (_mutatis mutandis_)
-example {α I : Type} (A B : I → Set α) : (⋂ i, A i ∩ B i) = (⋂ i, A i) ∩ ⋂ i, B i := by
-  sorry
-
-example {α I : Type} (A : I → Set α) (S : Set α) : (S ∩ ⋃ i, A i) = ⋃ i, A i ∩ S := by
-  sorry
-
-open Function
-
-variable (α β : Type) (f : α → β)
+example {α I : Type} (A B : I → Set α) : (⋂ i, A i ∩ B i) = (⋂ i, A i) ∩ ⋂ i, B i := sorry
 
 
--- The range is not *definitionally equal* to the image of the universal set:
-example : range f = f '' univ := by
-  sorry
+example {α I : Type} (A : I → Set α) (S : Set α) : (S ∩ ⋃ i, A i) = ⋃ i, A i ∩ S := sorry
+open Real
 
--- Why does this code *fail*? Fix it, and then prove the statement
-example (N : OddNaturals) : N ∈ Nat.succ '' (EvenNaturals) := sorry
+lemma frequently_one : ∃ᶠ (x : ℝ) in atTop, sin x = 1 := sorry
 
+lemma frequently_neg_one : ∃ᶠ (x : ℝ) in atTop, sin x = -1 := sorry
+    
 
--- Why does this code *fail*? Fix it, and then prove the statement
-example (N : OddNaturals) :  N ∈ Nat.succ ⁻¹' (EvenNaturals) := by sorry
+example (t : ℝ) : ¬ Tendsto (fun x ↦ Real.sin x) atTop (𝓝 t) := sorry
 
-
--- Not every `n : ℕ` is the successor or something...
-example : range Nat.succ ≠ univ := by
-  sorry
-
-
-/- The following is a *statement* and not merely the *definition* of being injective;
-  prove it. -/
-example : Injective f ↔ InjOn f univ := by
-  sorry
+/- If the sequence `u` converges to `x` and `u n` is in `M` for `n` big enough,
+then `x` is in the closure of `M`: a couple of useful lemmas, before:. -/
+#check mem_closure_iff_clusterPt
+#print ClusterPt
+#check le_principal_iff
+#check neBot_of_le
+-- Here you go!
+example (u : ℕ → ℝ) (M : Set ℝ) (x : ℝ) (hux : Tendsto u atTop (𝓝 x))
+    (huM : ∀ᶠ n in atTop, u n ∈ M) : x ∈ closure M := sorry
 
 
-/- With the obvious definition of surjective, state and prove the following result: the
- complement `Sᶜ` is referred to with the abbreviation `compl` in the library -/
-example : Surjective f ↔ (range f)ᶜ = ∅ := by
-  sorry
+example : ∀ᶠ x in nhds (0 : ℝ), |x| ≤ 1/2 := sorry
+
+/- There are rationals of the form `1/n` that are arbitrarily close to `0`.
+**ToDo** -/
+example : ∃ᶠ x in 𝓝 (0 : ℝ), ∃ n : ℤ, x = 1 / (n : ℝ) := sorry
 
 
 end Exercises
--- GradCourse26/5_Sets&Limits_lecture.md
+++ GradCourse26/5_Sets&Limits_lecture.md
@@ -135,68 +135,124 @@ def (S \ T : Set α) = fun a ↦ a ∈ S ∧ a ∉ T
 `⌘`
 +++
 
-# Functions
+# Filters
 
-## Introduction
+## Definition
 
-#### Functions among sets are different gadgets than functions among types.
+A filter `F` on a type `α` is set in `Set α` (*i. e.* a collection of sets in `α`) such that:
+1. The largest set `⊤ = Set.univ` is in `F`;
+2. If `s,t : Set α` and `s ⊆ t`, then `s ∈ F` implies that `t ∈ F` (they are "upwards closed")
+3. `F` is stable by finite intersections.
 
-Let's inspect the following code:
+More precisely, `Filter` is a structure:
+
 ```lean
-example (α β : Type) (S : Set α) (T : Set β) (f g : S → T) :
-    f = g ↔ ∀ a : α, a ∈ S → f a = g a :=
+structure Filter (α : Type*) : Type*
+  | sets : Set (Set α)
+  | univ_sets : univ ∈ self.sets
+  | sets_of_superset : ∀ {x y : Set α}, x ∈ sets → x ⊆ y → y ∈ sets
+  | inter_sets : ∀ {x y : Set α}, x ∈ sets → y ∈ sets → x ∩ y ∈ sets
 ```
-It *seems* to say that `f = g` if and only if they coincide on every element of the domain, yet... `⌘`
 
-+++ Take-home message
-To apply `f : α → β` to some `s ∈ S : Set α`, *restrict* it to the *subtype* `↑S` attached to `S`.
++++ Some examples of filters
+* Given a term `a : α`, the collection of all sets containing `a` is the **principal** filter (at `a`): this generalises to any set `S ⊆ α`, being the case `S = {a}`. It is denoted `𝓟 S`, typed `\MCP S`.
+
+* The collection of all sets of natural integers (or real numbers, or rational numbers...) that are
+  "large enough" or "small enough" are filters. They are called `atTop` and `atBot`, respectively.
+
+* In a topological space `X`, the collection of all neighbourhoods (*i. e.* sets containing an open neighbourhood) of a subspace `S` is a filter, denoted `𝓝 S`; when `S={x}`, we write `𝓝 x`.
+
+`⌘`
 
 +++
 
-## Some operations on sets and functions
+## Filters and properties
 
-+++ (Pre-)image, range, etc...
+If `F : Filter ℕ` is a filter on the naturals and `P : ℕ → Prop` is a property, we might want to say that
+  * `P` holds for every large enough `n` (*e.g*: satisfying $n^2 - 5 n ≥ 0$)
+  * that we can find arbitrary large `n` satisfying `P` (*e.g.*: being prime).
+  * We could also say that a function `f : ℝ → ℝ` satisfy `P : ℝ → Prop` "for all `x` sufficiently close to `1`" (*e.g.* being positive, when `f 1 > 0` and `f` is continuous).
 
-Given a function `f : α → β` and sets `(S : Set α), (T : Set β)`, we have:
++++ More generally
+Given `F : Filter α` and `P : α → Prop`, we write
+  * `∀ᶠ (a : α) in F, P a ↔ {a : α | P a} ∈ F`
+  * `∃ᶠ (a : α) in F, P a ↔ {a : α | ¬ P a} ∉ F`
+  
+  and we say that `P` holds **eventually** (*resp.* **frequently**) with respect to `F`. The above cases correpond to `atTop : Filter ℕ` and `𝓝 1 : Filter ℝ`.
 
-* The **image** of `S` through `f`, noted `f '' S`.
-This is the *set* `f '' S : Set β` whose defining property is
-```lean
-f '' S := fun b ↦ ∃ x, x ∈ S ∧ f x = b
-```
-Unfortunately it comes with a lot of accents (but we're in France...): and with a space between `f` and `''`: it is not `f'' S`, it is `f '' S`.
+  * Similarly, given `f g : α → β` and `F : Filter α` we write `f =ᶠ[F] g` and we say that `f` and `g` are asymptotically equal (along `F`) if
+  ```
+  {a : α | f a = g a} ∈ F
+  ```
++++
+## Limits
 
-* The **range** of `f`, equivalent to `f '' univ`.
-I write *equivalent* because the defining property is
-```lean
-range f := (fun b ↦ ∃ x, f x = b) : β → Prop = (Set β)
-```
-This is not the verbatim definition of `f '' univ` : there is an exercise about this.
+Filters are (among other things) a very convenient way to talk about **convergence**.
 
-* The **preimage** of `T` through `f`, denoted `f ⁻¹' T`.
-This is the set
-```lean
-f ⁻¹' T : Set α := fun a ↦ f a ∈ T
-```
-This also comes with one accent and _two_ spaces; the symbol `⁻¹` can be typed as `\^-1`.
+Consider a function $f : ℝ → ℝ$ and $a,b ∈ ℝ$. To say that
+$$
+\lim_{x → a} f (x) = b
+$$
+means
+$$
+∀\; ε > 0, ∃\; δ > 0 \;\text{ such that }\; ‖x - a‖ < δ ⇒  ‖f(x) - b‖ < ε
+$$
+or, equivalently,
+$$
+∀\; ε > 0, ∃\; δ > 0 \;\text{ such that }\; f (a - δ, a + δ) ⊆ (b - ε, b + ε).
+$$
+or, equivalently, that
+$$
+∀\; U_b ∈ 𝓝\; b, ∃\; V_a ∈ 𝓝\; a \text{ such that }V_a ⊆ f⁻¹ U_b.
+$$
+Upwards-closeness of filters makes the explicit description of $V_a$ useless: to require $V_a ⊆ f^{-1}U_b$ is the same as
 
-`⌘`
+    ∀ U : Set ℝ, U ∈  𝓝 b → f⁻¹' U ∈ 𝓝 a
+
+
+
+And the statement
+$\displaystyle{\lim_{x → +∞} f(x)=b}$ simply becomes
+
+    ∀ U : Set ℝ, U ∈  𝓝 b → f⁻¹' U ∈ (atTop : Filter ℝ)
+
++++ Is this translation really useful?
+
+Let $f,g : ℝ → ℝ$ and $a,b,c ∈ ℝ$. One theorem is that
+$$
+\lim_{x → a}f (x)=b ⇒ \lim_{y → b}g(y)= c ⇒ \lim_{x → a}(g∘ f)(x)=c
+$$
+while
+$$
+\lim_{x → +∞}f (x)=b ⇒ \lim_{y → b}g(y)= c ⇒ \lim_{x → +∞}(g∘ f)(x)=c
+$$
+is *another* theorem, because $+∞ ∉ ℝ$. And
+$$
+\lim_{x → a^-}f (x)=-∞ ⇒ \lim_{y → -\infty}g(y)= c ⇒ \lim_{x → a^-}(g∘ f)(x)=c
+$$
+is a third one. There are (at least) **5^3=125** such theorems.
 +++
 
-+++ Injective and Surjective functions
-The function `f` is **injective on `S`**, denoted by `InjOn f S` if it is injective (a notion defined for functions **between two types**) when restricted to `S`:
-```lean
-def : InjOn f S := ∀ x₁ ∈ S, ∀ x₂ ∈ S, f x₁ = f x₂ → x₁ = x₂
-```
++++ Filters as generalised sets
 
-In particular, the following equivalence is not a tautology:
-```lean
-example : Injective f ↔ InjOn f univ
-```
-rather, it is an exercise for you.
+( *Recall*: elements of `𝓟 s` = all sets
+containing `s`.)
 
-The obvious definition of *surjectivity* is also available...
+* #### `𝓟 s` replaces `s`, more general filters are "generalised sets" of `α`.
 
-`⌘`
+1. The **order** relation: sets on `α` are
+ordered by inclusion, so `S₁ ≤ S₂ ↔ S₁ ⊆ S₂ ↔ ∀ T, T ⊇ S₂ → T ⊇ S₁`. Hence:
+
+        def le (F G : Filter α) : F ≤ G ↔ ∀ t ∈ G, t ∈ F := Iff.rfl
+
+1. Image of a filter through a function `f : α → β`. This operation is called
+`Filter.map`:
+
+        theorem mem_map (t : Set β) (F : Filter α) : t ∈ F.map f ↔ f ⁻¹' t ∈ F := Iff.rfl
+
+3. With all this, the statement $\displaystyle{\lim_{x → a}f(x)=L}$ becomes
+
+       def Tendsto (f : α → β) (F : Filter α) (G : Filter β) :=
+          (𝓝 a).map f ≤ (𝓝 L)
 
 +++
